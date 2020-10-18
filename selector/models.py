4f45f4956db886b6a2454a9474c0c73f7e73eb90
samargarg <samargarg54@gmail.com>
2020-10-18 13:57:31 +0530

    Minor bugs

--- selector/models.py
+++ selector/models.py
@@ -1,7 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class UserAuth(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="tokens")
     access_token = models.CharField(max_length=500, blank=True, null=True)
     refresh_token = models.CharField(max_length=500, blank=True, null=True)
 
